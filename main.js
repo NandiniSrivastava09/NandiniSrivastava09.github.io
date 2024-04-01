474f95775528d8cac81753ce577494c55e678e42
NandiniSrivastava09 <158621322+NandiniSrivastava09@users.noreply.github.com>
2024-04-01 13:05:01 +0530

    x
    x

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Data Analysis Enthusiast", "Graphic Designer", "Web Developer"],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+  
